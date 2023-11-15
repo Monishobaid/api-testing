47ca7a8d94f1c1d70112e5632dbe71596b859a90
Monishobaid <monishobaid@gmail.com>
2023-11-16 01:22:25 +0530

    testing apis

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import BreakingBad from "./components/breakingBad.vue"
+</script>
+
+<template>
+  <div>hello world main application</div>
+  <Suspense>
+    <template #default>
+      <BreakingBad/>
+    </template>
+    <template #fallback>
+     <p>loading...</p>
+    </template>
+  </Suspense>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/breakingBad.vue
@@ -0,0 +1,17 @@
+<script setup>
+import axios from "axios";
+
+const response = await axios.get("https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json");
+console.log(response);
+</script>
+
+
+<template>
+    <h1>Hello world from breaking bad</h1>
+    <div v-for="cool in response">
+        <pre>{{cool.Results}}</pre>
+    </div>
+
+</template>
+<style>
+</style>
